775b8e753bbdc40491c2fc2c3be259804b3ff827
Jenny <gtg287y@gmail.com>
2017-10-15 23:11:04 -0400

    trying to center

--- web/scripts/main.dart
+++ web/scripts/main.dart
@@ -5,7 +5,6 @@ import 'dart:async';
 import 'dart:html';
 int width = 322;
 int height = 450;
-List<RenderedDoll> renderedDolls = new List<RenderedDoll>();
 Element div = querySelector("#output");
 Random rand = new Random();
 
@@ -25,20 +24,68 @@ Future<bool> drawDeck() async {
   await drawCard();
 }
 
-//TODO why is only one thing being drawn here.
-Future<bool>  drawCard() async{
-  CanvasElement monsterElement = new CanvasElement(width: 256, height: 206);
-  CanvasElement cardElement = new CanvasElement(width: width, height: height);
-  Doll doll = new HomestuckDoll();
-  renderedDolls.add(new RenderedDoll(cardElement, doll));
-  await Renderer.drawWhatever(cardElement, "images/blank.png");
+void drawTextBoxForRD(RenderedDoll rd) {
+
+}
+
+Future<CanvasElement>  drawMonster(Doll doll) async {
+  double monsterScale = 1.0;
+  CanvasElement monsterElement = new CanvasElement(width: (256*monsterScale).round(), height: (206*monsterScale).round());
+  Renderer.drawBG(monsterElement, ReferenceColours.BLACK, ReferenceColours.WHITE);
   await Renderer.drawDoll(monsterElement, doll);
-  Renderer.swapColors(cardElement, new Colour(rand.nextInt(255), rand.nextInt(255),rand.nextInt(255)));
+  return monsterElement;
+}
+
+Doll makeDoll()  {
+  Doll doll;
+  double randNum = rand.nextDouble();
+
+  if(randNum > 0.75) {
+    doll = new HomestuckDoll();
+  }else if (randNum > 0.5) {
+    doll = new HomestuckTrollDoll();
+  } else if (randNum > 0.25) {
+    doll = new ConsortDoll();
+  }else {
+    doll = new DenizenDoll();
+  }
+  return doll;
+}
+
+Future<CanvasElement> drawCardTemplate(Colour color) async{
+  CanvasElement cardElement = new CanvasElement(width: width, height: height);
+  await Renderer.drawWhatever(cardElement, "images/blank.png");
+  Renderer.swapColors(cardElement, color);
+  return cardElement;
+
+}
+
+Future<bool>  drawCard() async{
+    Colour color = new Colour(rand.nextInt(255), rand.nextInt(255),rand.nextInt(255));
+    CanvasElement cardElement = await drawCardTemplate(color);
+    Doll doll =  makeDoll();
+    RenderedDoll rd = new RenderedDoll(cardElement, doll);
+
+    drawTextBoxForRD(rd);
+    CanvasElement monsterElement = await drawMonster(doll);
+
 
 
   CanvasElement finishedProduct = new CanvasElement(width: width, height: height);
-  finishedProduct.context2D.drawImage(monsterElement, 25, 50);
+  Renderer.drawBG(finishedProduct, color, new Colour(255,255,255));
+  //TODO figure out where to draw monster based on monster size.
+  /*
+    take widht of monster box, divide by 2
+    width of doll /2
+    x = box/2 - doll/2
+    y = box_height/2 - doll_height/2
+   */
+  int x = (doll.width/2 - monsterElement.width/2).round();
+  int y = (3*monsterElement.height/4 - 2*monsterElement.height/4).round();
+  finishedProduct.context2D.drawImage(monsterElement, x, y);
   finishedProduct.context2D.drawImage(cardElement, 0, 0);
+
+
   div.append(finishedProduct);
 }
 
